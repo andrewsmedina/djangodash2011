8be463f8c826ce3d2e0d345846014184c25aecaa
Andrews Medina <andrewsmedina@gmail.com>
2011-07-30 00:34:01 -0300

    added url for index

--- startdust_website/urls.py
+++ startdust_website/urls.py
@@ -1,17 +1,13 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from panel.views import IndexView
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'startdust_website.views.home', name='home'),
+    url(r'^$', IndexView.as_view(), name='index'),
     # url(r'^startdust_website/', include('startdust_website.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
     (r'^accounts/', include('registration.backends.default.urls')),
 )
 
