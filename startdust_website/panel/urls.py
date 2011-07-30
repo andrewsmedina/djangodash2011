8ab4035af76645a025a292f24f0c0bd95f0ae36c
Andrews Medina <andrewsmedina@gmail.com>
2011-07-30 17:15:10 -0300

    fixed panel index url name

--- startdust_website/panel/urls.py
+++ startdust_website/panel/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, include, url
 from panel.views import IndexView
 
 urlpatterns = patterns('',
-    url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^$', IndexView.as_view(), name='panel-index'),
 
     (r'^projects/(?P<id_project>\d+)/$', 'panel.views.show_project'),
     (r'^projects/add/', 'panel.views.add_project'),
