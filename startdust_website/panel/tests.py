8ad4c6f3b004b88fb422ff8f08c3826dba13087d
Andrews Medina <andrewsmedina@gmail.com>
2011-07-30 12:52:42 -0300

    added tests and view for panel index

---
+++ startdust_website/panel/tests.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from django.test.client import RequestFactory
+from panel.views import IndexView
+
+
+class IndexViewTestCase(TestCase):
+
+    def setUp(self):
+        request = RequestFactory().get('/')
+        self.response = IndexView.as_view()(request)
+
+    def test_index_view_should_render_index_dot_html(self):
+        '''
+        index view should render panel/index.html
+        '''
+        self.assertIn('panel/index.html', self.response.template_name)
--- startdust_website/panel/views.py
+++ startdust_website/panel/views.py
@@ -1,1 +1,5 @@
+from django.views.generic import TemplateView
 
+
+class IndexView(TemplateView):
+    template_name = 'panel/index.html'
