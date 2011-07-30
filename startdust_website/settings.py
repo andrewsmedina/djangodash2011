76b5017b2ca835646bd7057255f08ae75db1082f
Andrews Medina <andrewsmedina@gmail.com>
2011-07-29 23:44:16 -0300

    settings for django registration

--- startdust_website/settings.py
+++ startdust_website/settings.py
@@ -119,8 +119,12 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'registration',
     'django_nose',
 )
+
+#django registration settings
+ACCOUNT_ACTIVATION_DAYS = 7
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
