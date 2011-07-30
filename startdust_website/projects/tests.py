f92c87b73d706c5c2d8262a74373dec8feb6ebda
Victor (Siminino) <victor.oliveira.ti@gmail.com>
2011-07-30 12:52:36 -0300

    Move project views for panel view

--- startdust_website/projects/tests.py
+++ startdust_website/projects/tests.py
@@ -6,9 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from django.test.client import RequestFactory
 from projects.models import Project
-from projects.views import add_project
 from projects.forms import ProjectForm
 
 
@@ -25,20 +23,6 @@ class ProjectModelTestCase(TestCase):
         self.assertEqual(expected_project.name, self.project.name)
         self.assertEqual(expected_project.url, self.project.url)
         self.assertEqual(expected_project.token, self.project.token)
-
-
-class ProjectViewTestCase(TestCase):
-
-    def setUp(self):
-        self.factory = RequestFactory()
-        request = self.factory.get('/painel/projects/add/')
-        self.response = add_project(request)
-
-    def test_add_project_should_return_status_code_200(self):
-        self.assertEqual(self.response.status_code, 200)
-
-    def test_add_project_should_have_form_on_context(self):
-        self.assertEqual(self.response.context_data['form'].__class__, ProjectForm)
 
 
 class ProjectFormTestCase(TestCase):
--- startdust_website/projects/views.py
+++ startdust_website/projects/views.py
@@ -1,15 +1,1 @@
-from django.template.response import TemplateResponse
-from projects.forms import ProjectForm
 
-def add_project(request):
-
-    if request.method == 'POST':
-        form = ProjectForm(request.POST)
-
-        if form.is_valid():
-            return
-
-    else:
-        form = ProjectForm()
-
-    return TemplateResponse(request, 'project_form.html', {'form': form})
