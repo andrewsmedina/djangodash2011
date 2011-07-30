1644f88bf7d3f20c26f86d0369f00406a35b4a73
Andrews Medina <andrewsmedina@gmail.com>
2011-07-30 12:09:35 -0300

    refactoring error tests for use assertFieldIn

--- startdust_website/errors/tests.py
+++ startdust_website/errors/tests.py
@@ -8,41 +8,25 @@ class ErrorModelTestCase(TestCase):
         '''
         error model should have a date attribue
         '''
-        for field in Error._meta.fields:
-            if field.name == 'date':
-                return
-
-        assert False
+        self.assertFieldIn('date', Error._meta.fields)
 
     def test_error_model_should_have_a_url_attribute(self):
         '''
         error model should have a url attribute
         '''
-        for field in Error._meta.fields:
-            if field.name == 'url':
-                return
-
-        assert False
+        self.assertFieldIn('url', Error._meta.fields)
 
     def test_error_model_should_have_a_traceback_attribute(self):
         '''
         error model should have a traceback attribute
         '''
-        for field in Error._meta.fields:
-            if field.name == 'traceback':
-                return
-
-        assert False
+        self.assertFieldIn('traceback', Error._meta.fields)
 
     def test_error_model_should_have_a_exception_attribute(self):
         '''
         error model should have a exception attribute
         '''
-        for field in Error._meta.fields:
-            if field.name == 'exception':
-                return
-
-        assert False
+        self.assertFieldIn('exception', Error._meta.fields)
 
     def assertFieldIn(self, expected_field, field_list):
         '''
