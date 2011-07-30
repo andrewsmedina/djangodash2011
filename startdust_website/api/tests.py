fcdbc04a5f888136211b1180df68ded0fd6b02b6
Andrews Medina <andrewsmedina@gmail.com>
2011-07-30 15:24:04 -0300

    view for api for add errors

--- startdust_website/api/tests.py
+++ startdust_website/api/tests.py
@@ -1,16 +1,25 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import RequestFactory
+from api.views import add_error
+from errors.models import Error
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ApiPostErrorViewTestCase(TestCase):
+
+    def test_api_error_post_view_should_add_a_error(self):
+        '''
+        api error post should add error
+        '''
+        post_data = {
+            'exception': 'some exception',
+            'url': 'http://someurl.com',
+            'traceback': 'some traceback',
+        }
+
+        request = RequestFactory().post('/api/errors/', post_data)
+        add_error(request)
+
+        try:
+            Error.objects.get(exception=post_data['exception'])
+        except Error.DoesNotExist:
+            assert False
--- startdust_website/api/views.py
+++ startdust_website/api/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from errors.models import Error
+from datetime import datetime
+
+
+def add_error(request):
+    if request.method == "POST":
+        error = Error.objects.create(
+            exception=request.POST['exception'],
+            date=datetime.now(),
+            url=request.POST['url'],
+            traceback=request.POST['traceback']
+        )
+    pass
