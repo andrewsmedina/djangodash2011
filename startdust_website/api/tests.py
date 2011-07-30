f4ba2307c95d85321ba05796d721856090558f30
Andrews Medina <andrewsmedina@gmail.com>
2011-07-30 15:38:51 -0300

    improved tests for 405 in add error

--- startdust_website/api/tests.py
+++ startdust_website/api/tests.py
@@ -32,3 +32,13 @@ class ApiPostErrorViewTestCase(TestCase):
         response = add_error(request)
 
         self.assertEqual(405, response.status_code)
+
+    def test_add_error_should_returns_methods_allowed_when_the_method_isnt_post(self):
+        '''
+        add_error should returns 405 status code when method inst a post
+        and returns post in method allowed
+        '''
+        request = RequestFactory().get('/api/errors/')
+        response = add_error(request)
+
+        self.assertTrue('post' in [method for method in response.items()[1]])
