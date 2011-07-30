5f5c96b4d2c8bf604b8f6a1721405b863fad7e29
Andrews Medina <andrewsmedina@gmail.com>
2011-07-30 19:45:17 -0300

    using dispatcher in middlware to send error to server

--- startdust_client/middleware.py
+++ startdust_client/middleware.py
@@ -1,3 +1,5 @@
+from dispatchers import send_error_to_server
+
 import traceback
 import sys
 
@@ -5,5 +7,7 @@ import sys
 class StartDustMiddleware(object):
 
     def process_exception(self, request, exception):
+        url = 'http://%s%s%s' % (request.META['SERVER_NAME'], ':' + request.META['SERVER_PORT'], request.path_info)
         exc_info = sys.exc_info()
-        #'\n'.join(traceback.format_exception(*(exc_info or sys.exc_info())))
+        trace = '\n'.join(traceback.format_exception(*(exc_info or sys.exc_info())))
+        send_error_to_server(exception.message, url, trace)
