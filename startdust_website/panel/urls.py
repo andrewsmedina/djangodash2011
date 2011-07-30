d285e6c02c7511d551c76d9a0c95b75529dac514
Andrews Medina <andrewsmedina@gmail.com>
2011-07-30 17:16:17 -0300

    using named urls

--- startdust_website/panel/urls.py
+++ startdust_website/panel/urls.py
@@ -3,7 +3,6 @@ from panel.views import IndexView
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='panel-index'),
-
-    (r'^projects/(?P<id_project>\d+)/$', 'panel.views.show_project'),
-    (r'^projects/add/', 'panel.views.add_project'),
+    url(r'^projects/(?P<id_project>\d+)/$', 'panel.views.show_project', name='project'),
+    url(r'^projects/add/', 'panel.views.add_project', name='add-project'),
 )
