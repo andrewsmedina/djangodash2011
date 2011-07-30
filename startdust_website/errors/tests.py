7862efb8a9de8c2cbb90c3e7287df1fc6a11cd18
Andrews Medina <andrewsmedina@gmail.com>
2011-07-30 11:36:48 -0300

    fixed a typo in error tests

--- startdust_website/errors/tests.py
+++ startdust_website/errors/tests.py
@@ -4,11 +4,13 @@ from errors.models import Error
 
 class ErrorModelTestCase(TestCase):
 
-    def test_error_should_have_a_date_attribute(self):
+    def test_error_model_should_have_a_date_attribute(self):
         '''
         error model should have a date attribue
         '''
         for field in Error._meta.fields:
-            return field.name == 'data'
+            if field.name == 'date':
+                return
 
         assert False
+
