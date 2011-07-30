21327402a03961fbf002f307f901fd0356916acd
Andrews Medina <andrewsmedina@gmail.com>
2011-07-30 00:30:00 -0300

    test and view for panel index

--- startdust_website/panel/tests.py
+++ startdust_website/panel/tests.py
@@ -1,16 +1,15 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test.client import RequestFactory
+from panel.views import IndexView
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class IndexViewTestCase(TestCase):
+
+    def test_index_view_should_render_base_dot_html(self):
+        '''
+        index view should render panel/base.html
+        '''
+        request = RequestFactory().get('/')
+        response = IndexView.as_view()(request)
+
+        self.assertIn('panel/base.html', response.template_name)
--- startdust_website/urls.py
+++ startdust_website/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
@@ -16,3 +14,5 @@ urlpatterns = patterns('',
     # url(r'^admin/', include(admin.site.urls)),
     (r'^accounts/', include('registration.backends.default.urls')),
 )
+
+urlpatterns += staticfiles_urlpatterns()
