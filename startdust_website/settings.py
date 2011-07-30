61c356305136e9e872696293f74451551f18c7b7
Andrews Medina <andrewsmedina@gmail.com>
2011-07-29 23:54:29 -0300

    added panel in installed apps

--- startdust_website/settings.py
+++ startdust_website/settings.py
@@ -115,10 +115,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+
+    'panel',
     'registration',
     'django_nose',
 )
