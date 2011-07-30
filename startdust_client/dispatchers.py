c9d586078672568cf6411bf391feefbdf7156d77
Andrews Medina <andrewsmedina@gmail.com>
2011-07-30 18:34:04 -0300

    added dispatcher to send response to stardust server

--- startdust_client/dispatchers.py
+++ startdust_client/dispatchers.py
@@ -1,7 +1,7 @@
 import urllib
 
 
-STARDUST_URL = 'http://localhost:8000/api/error/'
+STARDUST_URL = 'http://localhost:8000/api'
 
 
 def send_error_to_server(exception, url, traceback):
@@ -15,5 +15,19 @@ def send_error_to_server(exception, url, traceback):
     }
 
     post_dict = urllib.urlencode(post_dict)
-    response = urllib.urlopen(STARDUST_URL, post_dict).read()
+    response = urllib.urlopen('%s/error/' % STARDUST_URL, post_dict).read()
+    return response
+
+
+def send_response_to_server(url, time):
+    '''
+    send response to stardust server
+    '''
+    post_dict = {
+        'url': url,
+        'time': time,
+    }
+
+    post_dict = urllib.urlencode(post_dict)
+    response = urllib.urlopen('%s/response/' % STARDUST_URL, post_dict).read()
     return response
