d3cc19e193300e70dda6bb22ddc02e85e56d91d0
Andrews Medina <andrewsmedina@gmail.com>
2011-07-31 14:58:28 -0300

    refactored dispatcher tests

--- startdust_client/test.py
+++ startdust_client/test.py
@@ -2,17 +2,15 @@ from unittest import TestCase
 from dispatchers import Dispatcher 
 
 
-class SendErrorToServerTestCase(TestCase):
+class DispatcherTestCase(TestCase):
+
+    def setUp(self):
+        self.dispatcher = Dispatcher('andrews', 'andrews', 'sometoken')
 
     def test_send_error_to_server(self):
-        dispatcher = Dispatcher('andrews', 'andrews', 'sometoken')
-        response = dispatcher.send_error('some excpetion', 'http://someurl.com', 'some traceback')
+        response = self.dispatcher.send_error('some excpetion', 'http://someurl.com', 'some traceback')
         self.assertEqual('error added with success!', response)
 
-
-class SendResponseToServerTestCase(TestCase):
-
     def test_send_response_to_server(self):
-        dispatcher = Dispatcher('andrews', 'andrews', 'sometoken')
-        response = dispatcher.send_response('http://someurl.com', 1.234)
+        response = self.dispatcher.send_response('http://someurl.com', 1.234)
         self.assertEqual('response added with success!', response)
