b887248707f355765decf380a1df29215ac2dd8f
Andrews Medina <andrewsmedina@gmail.com>
2011-07-31 16:29:44 -0300

    tests for docs view

--- startdust_website/home/tests.py
+++ startdust_website/home/tests.py
@@ -21,3 +21,23 @@ class IndexViewTestCase(TestCase):
         '''
         response = self.client.get('/')
         self.assertEqual(200, response.status_code)
+
+
+class DocsViewTestCase(TestCase):
+
+    def setUp(self):
+        request = RequestFactory().get('/')
+        self.response = DocsView.as_view()(request)
+
+    def test_docs_view_should_render_home_dot_html(self):
+        '''
+        docs view should render home/docs.html
+        '''
+        self.assertIn('home/docs.html', self.response.template_name)
+
+    def test_docs_url_should_be_returns_200_how_status_code(self):
+        '''
+        url for docs view should be returns 200 in status code
+        '''
+        response = self.client.get('/docs/')
+        self.assertEqual(200, response.status_code)
