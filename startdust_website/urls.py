d41c2c2446b26f8adadc2e1cafdbe5f200983c07
Andrews Medina <andrewsmedina@gmail.com>
2011-07-31 16:39:09 -0300

    added features in urls.py

--- startdust_website/urls.py
+++ startdust_website/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from home.views import IndexView, DocsView
+from home.views import IndexView, DocsView, FeaturesView
+
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
     url(r'^docs/$', DocsView.as_view(), name='docs'),
+    url(r'^features/$', FeaturesView.as_view(), name='features'),
 
     (r'^api/', include('api.urls')),
     (r'^panel/', include('panel.urls')),
