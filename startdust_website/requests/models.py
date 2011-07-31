bf80bf55d951246e539f4cfad7d89dc04ae66ef3
Andrews Medina <andrewsmedina@gmail.com>
2011-07-31 19:16:02 -0300

    add datetime in requests

--- startdust_website/requests/models.py
+++ startdust_website/requests/models.py
@@ -6,8 +6,13 @@ from projects.models import Project
 class Request(models.Model):
 
     url = models.URLField(verify_exists=False)
-    date = models.DateTimeField(default=datetime.now, editable=False)
+    date = models.DateTimeField(null=True, editable=False)
     project = models.ForeignKey(Project, null=True, editable=False)
 
     class Meta:
         ordering = ['date']
+
+    def save(self, *args, **kwargs):
+        date = datetime.now()
+        self.date = datetime(date.year, date.month, date.day, date.hour, date.minute)
+        super(Request, self).save(*args, **kwargs)
