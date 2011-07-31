a175d3f0f0d155d5ea320b98874720363968b9d6
Andrews Medina <andrewsmedina@gmail.com>
2011-07-31 16:32:29 -0300

    docs url

--- startdust_website/urls.py
+++ startdust_website/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from home.views import IndexView
+from home.views import IndexView, DocsView
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='index'),
+    url(r'^docs/$', DocsView.as_view(), name='docs'),
 
     (r'^api/', include('api.urls')),
     (r'^panel/', include('panel.urls')),
