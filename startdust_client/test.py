74bfcd065b2a8bfdbd0b3764f469f40ef13034e3
Andrews Medina <andrewsmedina@gmail.com>
2011-07-31 14:59:27 -0300

    tests for new dispatcher (request dispatcher)

--- startdust_client/test.py
+++ startdust_client/test.py
@@ -7,6 +7,10 @@ class DispatcherTestCase(TestCase):
     def setUp(self):
         self.dispatcher = Dispatcher('andrews', 'andrews', 'sometoken')
 
+    def test_send_request_to_server(self):
+        response = self.dispatcher.send_request('http://someurl.com')
+        self.assertEqual('request added with success!', response)
+
     def test_send_error_to_server(self):
         response = self.dispatcher.send_error('some excpetion', 'http://someurl.com', 'some traceback')
         self.assertEqual('error added with success!', response)
