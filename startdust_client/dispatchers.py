11104d6bda72d57ed46bb707fa804a8c84fb3230
Andrews Medina <andrewsmedina@gmail.com>
2011-07-31 00:07:16 -0300

    changed dispachers to be a object

--- startdust_client/dispatchers.py
+++ startdust_client/dispatchers.py
@@ -6,37 +6,38 @@ import base64
 STARDUST_URL = 'http://localhost:8000/api'
 
 
-def send_error_to_server(exception, url, traceback):
-    '''
-    send error message to stardust server
-    '''
-    post_dict = {
-        'exception': exception,
-        'url': url,
-        'traceback': traceback,
-    }
-    request = urllib2.Request('%s/error/' % STARDUST_URL)
-    base64string = base64.encodestring('%s:%s' % ('andrews', 'andrews'))[:-1]
-    authheader =  "Basic %s" % base64string
-    request.add_header("Authorization", authheader)
-    post_dict = urllib.urlencode(post_dict)
-    response = urllib2.urlopen(request, post_dict).read()
-    return response
+class Dispatcher(object):
 
+    def send_error(self, exception, url, traceback):
+        '''
+        send error message to stardust server
+        '''
+        post_dict = {
+            'exception': exception,
+            'url': url,
+            'traceback': traceback,
+        }
+        request = urllib2.Request('%s/error/' % STARDUST_URL)
+        base64string = base64.encodestring('%s:%s' % ('andrews', 'andrews'))[:-1]
+        authheader =  "Basic %s" % base64string
+        request.add_header("Authorization", authheader)
+        post_dict = urllib.urlencode(post_dict)
+        response = urllib2.urlopen(request, post_dict).read()
+        return response
 
-def send_response_to_server(url, time):
-    '''
-    send response to stardust server
-    '''
-    post_dict = {
-        'url': url,
-        'time': time,
-    }
-    request = urllib2.Request('%s/response/' % STARDUST_URL)
-    base64string = base64.encodestring('%s:%s' % ('andrews', 'andrews'))[:-1]
-    authheader =  "Basic %s" % base64string
-    request.add_header("Authorization", authheader)
+    def send_response(self, url, time):
+        '''
+        send response to stardust server
+        '''
+        post_dict = {
+            'url': url,
+            'time': time,
+        }
+        request = urllib2.Request('%s/response/' % STARDUST_URL)
+        base64string = base64.encodestring('%s:%s' % ('andrews', 'andrews'))[:-1]
+        authheader =  "Basic %s" % base64string
+        request.add_header("Authorization", authheader)
 
-    post_dict = urllib.urlencode(post_dict)
-    response = urllib2.urlopen(request, post_dict).read()
-    return response
+        post_dict = urllib.urlencode(post_dict)
+        response = urllib2.urlopen(request, post_dict).read()
+        return response
--- startdust_client/test.py
+++ startdust_client/test.py
@@ -1,16 +1,18 @@
 from unittest import TestCase
-from dispatchers import send_error_to_server, send_response_to_server
+from dispatchers import Dispatcher 
 
 
 class SendErrorToServerTestCase(TestCase):
 
     def test_send_error_to_server(self):
-        response = send_error_to_server('some excpetion', 'http://someurl.com', 'some traceback')
+        dispatcher = Dispatcher()
+        response = dispatcher.send_error('some excpetion', 'http://someurl.com', 'some traceback')
         self.assertEqual('error added with success!', response)
 
 
 class SendResponseToServerTestCase(TestCase):
 
     def test_send_response_to_server(self):
-        response = send_response_to_server('http://someurl.com', 1.234)
+        dispatcher = Dispatcher()
+        response = dispatcher.send_response('http://someurl.com', 1.234)
         self.assertEqual('response added with success!', response)
