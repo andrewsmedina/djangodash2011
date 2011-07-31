99b28f9ddd84520785c2bfe12ea8e78845ce2f56
Andrews Medina <andrewsmedina@gmail.com>
2011-07-31 02:13:18 -0300

    initial setup for stardust client

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(name='stardust',
+      version='xpto',
+      description='some description',
+      author='cobrateam',
+      author_email='andrewsmedina@gmail.com',
+      packages=find_packages(),
+      include_package_data=True,
+      )
