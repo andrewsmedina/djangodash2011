4e46ef3c668567a3f9ad8dc2091b550b44c872b5
Andrews Medina <andrewsmedina@gmail.com>
2011-07-31 14:41:56 -0300

    added request resource in api urls

--- startdust_website/api/urls.py
+++ startdust_website/api/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.defaults import patterns, include, url
 from piston.resource import Resource
 from piston.authentication import HttpBasicAuthentication
-from api.handlers import ErrorHandler, ResponseHandler
+from api.handlers import ErrorHandler, ResponseHandler, RequestHandler
 
 
 auth = HttpBasicAuthentication(realm="stardust")
@@ -9,12 +9,15 @@ ad = {'authentication': auth}
 
 error_resource = Resource(handler=ErrorHandler, **ad)
 response_resource = Resource(handler=ResponseHandler, **ad)
+request_resource = Resource(handler=RequestHandler, **ad)
 
 error_resource.csrf_exempt = True
 response_resource.csrf_exempt = True
+request_resource.csrf_exempt = True
 
 
 urlpatterns = patterns('',
     url(r'^error/$', error_resource),
     url(r'^response/$', response_resource),
+    url(r'^request/$', request_resource),
 )
